95e4dc26090b71f6dfeab1945a88a09f7a8fdb1c
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-11-28 19:58:55 +0100

    frontend implemented

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,4 +1,5 @@
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+// Remember: pass BootstrapContext to bootstrapApplication to avoid NG0401.
+import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
